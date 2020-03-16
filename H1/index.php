62a7fe46106ff90fb75fe75b1cf3307286439ef0
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2020-03-16 17:17:19 +0100

    H1 V2.5

---
+++ H1/index.php
@@ -0,0 +1,40 @@
+<?php
+$a = "Ik heb niet gefaald.";
+$b = "die niet werken";
+$c = 10000;
+
+echo nl2br ($a." Ik heb alleen ".$c." manieren gevonden die niet werken. \n -Thomas Edison") ;
+?>
+
+<br>
+<br>
+
+<?php
+$a = "die geen fout";
+$b = "en";
+$c = "niet";
+
+echo nl2br ("mensen ".$a."en maken werken ".$c." \n -Albert Plesman");
+?>
+
+<br>
+<br>
+
+<?php
+$a = "ry";
+$b = "but";
+$c = "fail";
+
+echo nl2br  ("t".$a." and ".$c.", ".$b." never ".$c." to t".$a. "\n -Tyra Banks");
+?>
+
+<br>
+<br>
+
+<?php
+$a = "\"omgaan";
+$b = "met";
+$c = "teleu";
+
+echo nl2br  ("De cursus ".$a." ".$b." ".$c."rstellingen"."\""." Kan vanavond helaas niet doorgaan. \n - Herman Finkers");
+?>
