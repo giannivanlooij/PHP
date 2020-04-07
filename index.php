06c195bf15c07ab8b8d8e9cbc1fa7010c95e396b
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2020-04-07 15:43:41 +0200

    index update

--- index.php
+++ index.php
@@ -19,7 +19,8 @@
     <li><a href="H4/index.php">Hoofdstuk 4</a></li>
     <li><a href="H5/H5.1/form.html">Hoofdstuk 5.1</a></li>
     <li><a href="H5/H5.2/form.html">Hoofdstuk 5.2</a></li>
-    <li><a href="H5/H5.3/form.html">Hoofdstuk 5.3</a></li>
+    <li><a href="H5/H5.3/form.html">Hoofdstuk 5.3 & 5.4</a></li>
+    <li><a href="H6/H6.1.php">Hoofdstuk 6</a></li>
     </ul>
 </body>
 </html>
